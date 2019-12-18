bade517cde5b459dbf29f566c8d3b32a7c57cfae
my <123456@163.com>
2019-12-18 09:33:25 +0800

    first

---
+++ src/main/java/com/my/GitApp.java
@@ -0,0 +1,11 @@
+package com.my;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitApp {
+
+    public static void main(String[] args) {
+        System.out.println("这是第一条数据");
+    }
+}
